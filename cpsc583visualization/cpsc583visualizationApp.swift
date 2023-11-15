b7fc2930096a03fc6bcbf16f379afbe7de3a2b4c
lauryn-anderson <laurynelysse@gmail.com>
2023-11-15 14:21:34 -0700

    Initial Commit

---
+++ cpsc583visualization/cpsc583visualizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  cpsc583visualizationApp.swift
+//  cpsc583visualization
+//
+//  Created by Lauryn Anderson on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct cpsc583visualizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
